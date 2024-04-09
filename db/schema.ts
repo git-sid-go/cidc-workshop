645c6b66291f62fbce345c3474bffcf726ac3aa7
Siddharth Srinivasan <siddhuvaradan@gmail.com>
2024-04-09 15:12:36 +0530

    complete schema and seeding

--- db/schema.ts
+++ db/schema.ts
@@ -1,8 +1,9 @@
-import { sql } from "drizzle-orm";
-import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { relations, sql } from "drizzle-orm";
+import { primaryKey, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { v4 as uuidv4 } from "uuid";
 
 export const users = sqliteTable("users", {
-  id: text("id").notNull().primaryKey(),
+  id: text("id").default(uuidv4()).primaryKey(),
   name: text("name").notNull(),
   email: text("email").notNull().unique(),
   createdAt: text("created_at")
@@ -12,3 +13,53 @@ export const users = sqliteTable("users", {
     .notNull()
     .default(sql`CURRENT_TIMESTAMP`),
 });
+
+export const usersRelations = relations(users, ({ many }) => ({
+  usersToSkills: many(usersToSkills),
+}));
+
+export const skills = sqliteTable("skills", {
+  id: text("id").default(uuidv4()).primaryKey(),
+  name: text("name").notNull(),
+  icon: text("icon_url").notNull(),
+  createdAt: text("created_at")
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+  updatedAt: text("updated_at")
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+});
+
+export const skillsRelations = relations(skills, ({ many }) => ({
+  usersToSkills: many(usersToSkills),
+}));
+
+export const usersToSkills = sqliteTable(
+  "users_to_skills",
+  {
+    userId: text("user_id")
+      .references(() => users.id)
+      .notNull(),
+    skillId: text("skill_id")
+      .references(() => skills.id)
+      .notNull(),
+    proficiency: text("proficiency", {
+      enum: ["beginner", "intermediate", "expert"],
+    }),
+  },
+  (t) => ({
+    pk: primaryKey({ columns: [t.userId, t.skillId] }),
+  })
+);
+
+export const userToSkillsRelations = relations(usersToSkills, ({ one }) => ({
+  skill: one(skills, {
+    fields: [usersToSkills.skillId],
+    references: [skills.id],
+  }),
+
+  user: one(users, {
+    fields: [usersToSkills.userId],
+    references: [users.id],
+  }),
+}));
--- db/seed.ts
+++ db/seed.ts
@@ -13,11 +13,19 @@ const client = createClient({
 const db = drizzle(client, { schema });
 
 const TOTAL_USERS = 100;
+const TOTAL_SKILLS = 50;
+const TOTAL_SKILLS_TO_ADD = 5;
 
 async function main() {
   console.log("Start seeding...");
 
   const usersToPush = [];
+
+  const skillsToPush = [];
+
+  const usersToSkillsToPush = [];
+
+  const allProficiencies = ["beginner", "intermediate", "expert"] as const;
 
   for (let i = 0; i < TOTAL_USERS; i++) {
     const user = {
@@ -29,13 +37,50 @@ async function main() {
     usersToPush.push(user);
   }
 
-  const users = await db
+  const usersFromDb = await db
     .insert(schema.users)
     .values(usersToPush)
     .onConflictDoNothing()
     .returning();
 
-  console.log("Users seeded.", { users });
+  for (let i = 0; i < TOTAL_SKILLS; i++) {
+    const skill = {
+      id: faker.database.mongodbObjectId(),
+      name: faker.person.jobTitle(),
+      icon: faker.image.avatar(),
+    };
+
+    skillsToPush.push(skill);
+  }
+
+  const skillsFromDb = await db
+    .insert(schema.skills)
+    .values(skillsToPush)
+    .onConflictDoNothing()
+    .returning();
+
+  for (let i = 0; i < usersFromDb.length; i++) {
+    const user = usersFromDb[i];
+    let skillsIds: string[] = [];
+
+    for (let j = 0; j < TOTAL_SKILLS_TO_ADD; j++) {
+      const skillId =
+        skillsFromDb[Math.floor(Math.random() * (skillsFromDb.length - 1))].id;
+
+      skillsIds = Array.from(new Set([...skillsIds, skillId]));
+    }
+
+    for (let skillIndex = 0; skillIndex < skillsIds.length; skillIndex++) {
+      usersToSkillsToPush.push({
+        userId: user.id,
+        skillId: skillsIds[skillIndex],
+        proficiency:
+          allProficiencies[Math.floor(Math.random() * allProficiencies.length)],
+      });
+    }
+  }
+
+  await db.insert(schema.usersToSkills).values(usersToSkillsToPush);
 }
 
 main();
